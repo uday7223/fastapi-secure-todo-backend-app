d7796b15ccd1f6a8cb14110c7fec5c1f4446a923
udaydevanur <uday.devnur223@gmail.com>
2025-06-25 20:14:29 +0530

    fix changes to env for deploy

--- auth.py
+++ auth.py
@@ -7,14 +7,20 @@ load_dotenv()  # Load from .env
 
 
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+# SECRET_KEY = os.getenv("SECRET_KEY")
+# ALGORITHM = os.getenv("ALGORITHM")
+# ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+
+
+SECRET_KEY = os.environ.get("DATABASE_URL")
+ALGORITHM = os.environ.get("ALGORITHM")
+
+
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=60)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
