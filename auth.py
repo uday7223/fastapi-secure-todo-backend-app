d9d0712b689a059bcdc7a21241c2e141bb46a72f
udaydevanur <uday.devnur223@gmail.com>
2025-06-25 15:01:17 +0530

    setup succefully for deploying on railway

--- auth.py
+++ auth.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load from .env
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
