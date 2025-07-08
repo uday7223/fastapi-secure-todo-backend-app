577af5bbe2fef6c5ec6c4cf26776a4abab771bb0
uday7223 <uday.devnur223@gmail.com>
2025-07-08 23:19:15 +0530

    set the token expiry for 1 day

--- auth.py
+++ auth.py
@@ -20,7 +20,7 @@ ALGORITHM = os.environ.get("ALGORITHM")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=60)
+    expire = datetime.utcnow() + timedelta(minutes=1440)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
