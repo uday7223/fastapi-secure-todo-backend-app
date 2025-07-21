656a054e669100172450fd1746a4be802638127e
uday7223 <uday.devnur223@gmail.com>
2025-07-21 10:14:30 +0530

    fix a test case scenario

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class Todo(Base):
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    username = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     # Inside User class
